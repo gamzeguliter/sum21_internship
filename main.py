17a9de8323631d0ed0567974926f1d7662daa109
gamzeguliter <gamze.guliter@ug.bilkent.edu.tr>
2021-08-02 21:08:17 +0300

    last update

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ with open('flight.csv', 'r') as file:
     for row in reader:
         count = count + 1
         if count != 1 :
-            tempFlight = flight(row[0],row[1],row[2],row[3],row[4])
+            tempFlight = flight(row[0],row[2],row[1],row[3],row[4])
             Flights.append(tempFlight)
 
 i = 0
@@ -53,15 +53,15 @@ pass
 def create_randomPlanes(planeCount):
     i = 0
     while i < 3:
-        p = plane("p"+str(i),0,None,"ANK")
+        p = plane("p"+str(i),1,None,"ANK")
         Planes.append(p)
         i = i + 1
     while i < 7:
-        p = plane("p"+str(i),0,None,"IST")
+        p = plane("p"+str(i),1,None,"IST")
         Planes.append(p)
         i = i + 1
     while i < 10:
-        p = plane("p"+str(i),0,None,"NY")
+        p = plane("p"+str(i),1,None,"NY")
         Planes.append(p)
         i = i + 1
 
@@ -73,19 +73,43 @@ pass
 
 def schedule_flights(args,args2):
     hour = 0
-    counter = 0;
+    counter = 0
+    time = 0
 
     while hour < 24:
         minute = 0
         while minute < 60:
 
             minute = minute + 1
-            counter = counter + 1
+            counter = counter + 1      #end of one minute
+            if minute < 10 :
+                time = hour + minute * (0.1)
+               # print(time)
+            else:
+               time = hour + minute * (0.01)
+              # print(time)
+            tempCounter = 0
+            for a in args:
+                if float(a.leavingTime) == time:
+                    #just assign first 10 planes for now
+                        for x in args2:
+                            if  tempCounter <10:
+                                if x.available == 1:
+                                    x.flight = a
+                                    x.available = 0
+                                    print(time, x.flight.name,x.name,"FLIGHT")
+                                    break
+                                tempCounter =  tempCounter +1 
 
-        hour = hour + 1
 
 
-    print(counter)
+
+        hour = hour + 1 #end of one hour
+
+
+
+
+
 
 pass
 
--- plane.py
+++ plane.py
@@ -3,6 +3,6 @@ class plane:
     def __init__(self,name,available,flight,place):
         self.name = name
         self.place = place
-        self.avilable = available
+        self.available = available
         self.flight = flight
         pass
